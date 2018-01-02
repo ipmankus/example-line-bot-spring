3ac75c04fe23c074e4f132303c40fb242cf8f726
Pande Ketut Cahya Nugraha <32126651+anthrocoon12@users.noreply.github.com>
2018-01-02 11:26:48 +0700

    Update ExampleBotApplication.java

--- src/main/java/example/ExampleBotApplication.java
+++ src/main/java/example/ExampleBotApplication.java
@@ -2,6 +2,7 @@ package example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.linecorp.bot.client.LineMessagingClient;
 import com.linecorp.bot.model.profile.UserProfileResponse;
