5e1ac30533b1e90382f88667ca558e6133735069
Pande Ketut Cahya Nugraha <32126651+anthrocoon12@users.noreply.github.com>
2018-01-02 11:23:11 +0700

    Update ExampleBotApplication.java

--- src/main/java/example/ExampleBotApplication.java
+++ src/main/java/example/ExampleBotApplication.java
@@ -3,6 +3,8 @@ package example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.linecorp.bot.client.LineMessagingClient;
+import com.linecorp.bot.model.profile.UserProfileResponse;
 import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
@@ -13,14 +15,30 @@ import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 @SpringBootApplication
 @LineMessageHandler
 public class ExampleBotApplication {
+    @Autowired
+    private LineMessagingClient lineMessagingClient;
+
     public static void main(String args[]) {
         SpringApplication.run(ExampleBotApplication.class, args);
     }
 
     @EventMapping
-    public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) throws Exception {
-        String message = event.getMessage().getText();
-        String reply = message + " - ExampleBot";
-        return new TextMessage(reply);
+    public void handleTextMessageEvent(MessageEvent<TextMessageContent> event) throws Exception {
+        String message = event.getMessage();
+        handleTextContent(event.getReplyToken(), event, message);
+    }
+
+    private TextMessage handleTextContent(String replyToken, javafx.event.Event event, TextMessageContent content) {
+        String text = content.getText().toLowerCase();
+
+        if (text == "profile") {
+            String userId = event.getSource().getUserId();
+            UserProfileResponse userProfile = lineMessagingClient.getProfile(userId);
+            String displayName = userProfile.getDisplayName();
+            String statusMessage = userProfile.getStatusMessage();
+
+            String reply = displayName + "\nStatus : " + statusMessage;
+            return new TextMessage(reply)
+        }        
     }
 }
